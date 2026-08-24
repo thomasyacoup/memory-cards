4f324622b34b6cbea2d256ec2316ceca40e2eb34
thomas011001 <thomasyacoub321@gmail.com>
2026-08-24 03:05:31 +0300

    refactor: remove routing and related components, simplify app structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { RouterProvider } from "react-router-dom";
-import routes from "./routes.jsx";
 import "./styles/index.css";
+import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={routes} />
+    <App />
   </StrictMode>
 );
