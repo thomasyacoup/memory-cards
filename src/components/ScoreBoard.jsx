c84260fa6d1768225b8c0d9c2e097b15855a890f
thomas011001 <thomasyacoub321@gmail.com>
2026-08-25 09:18:30 +0300

    refactor: restructure ScoreBoard component to include Title and Score subcomponents

--- src/components/ScoreBoard.jsx
+++ src/components/ScoreBoard.jsx
@@ -1,12 +1,64 @@
-function ScoreBoard({ score, highestScore, reset }) {
+import { css, cva } from "../../styled-system/css";
+
+function Title({children}) {
+  const style = css({
+    fontSize: "x-small",
+    fontWeight: "bold",
+    textTransform: "uppercase",
+    opacity: "0.8"
+  })
+  
   return (
-    <div className="score-board">
-      <div>
-        Current: {score} Highest: {highestScore}
-      </div>
-      <button onClick={reset}>Reset</button>
+    <h3 className={style}>{children}</h3>
+  )
+}
+
+function Score({children}) {
+  const style = css({
+    fontSize: "lg",
+    fontFamily: "mono",
+    fontWeight: "bold"
+  })
+  
+  return (
+    <p className={style}>{children}</p>
+  )
+}
+
+
+function ScoreBoard({ children, variant }) {
+  const style = cva({
+    base: {
+      display: "flex",
+      flexDir: "column",
+      border: "3px solid black",
+      shadow: "3px 3px 0px 1px black",
+      justifyContent: "center",
+      px: "4",
+      textAlign: "center",
+    },
+    variants: {
+      variant: {
+        primary: {
+          bg: "yellow.300",
+          color: "black"
+        },
+        muted: {
+          bg: "neutral.800",
+          color: "white"
+        }
+      }
+    }
+  })
+  
+  return (
+    <div className={style({variant: variant})}>
+      {children}
     </div>
   );
 }
+
+ScoreBoard.Title = Title
+ScoreBoard.Score = Score
 
 export default ScoreBoard;
