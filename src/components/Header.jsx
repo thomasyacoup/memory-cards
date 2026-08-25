adfa83c1a0b1e308343a66e51e88a66db7fb98b8
thomas011001 <thomasyacoub321@gmail.com>
2026-08-25 09:19:38 +0300

    feat: add Header component and remove NavBar component for improved layout

---
+++ src/components/Header.jsx
@@ -0,0 +1,27 @@
+import { css, cx } from "../../styled-system/css"
+import { container } from "../../styled-system/patterns"
+import ScoreBoard from "./ScoreBoard"
+
+function Header({level, streak, remainCards}) {
+  return (
+    <header className={cx(css({display: "flex", py: "2",  shadow: "7px 7px 0px 1px black", border: "3px solid black", justifyContent: "space-between"}), container())}>
+      <h1 className={css({color: "white", fontWeight: "bold", fontSize: "4xl", fontStyle: "italic"})}>Memory Cards</h1>
+      <div className={css({display: "flex", gap: "4"})}>
+        <ScoreBoard variant="muted">
+          <ScoreBoard.Title>Remaining</ScoreBoard.Title>
+          <ScoreBoard.Score>{remainCards}</ScoreBoard.Score>
+        </ScoreBoard>
+        <ScoreBoard variant="muted">
+          <ScoreBoard.Title>Level</ScoreBoard.Title>
+          <ScoreBoard.Score>{level}</ScoreBoard.Score>
+        </ScoreBoard>
+        <ScoreBoard variant="primary">
+          <ScoreBoard.Title>Streak</ScoreBoard.Title>
+          <ScoreBoard.Score>{streak}</ScoreBoard.Score>
+        </ScoreBoard>
+      </div>
+    </header>
+  )
+}
+
+export default Header
