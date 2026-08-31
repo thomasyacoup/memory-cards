446a6058db2fa3d7ad7622efcf8aa4eb3cf81bc9
thomas011001 <thomasyacoub321@gmail.com>
2026-08-31 23:48:37 +0300

    fix: adjust Header component styles for better alignment and readability

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,8 +4,8 @@ import ScoreBoard from "./ui/ScoreBoard"
 
 function Header({level, streak, remainCards}) {
   return (
-    <header className={cx(css({w: "full", display: "flex", py: "2", shadow: "7px 7px 0px 1px black", border: "3px solid black", justifyContent: "space-between"}), container())}>
-      <h1 className={css({color: "white", fontWeight: "bold", fontSize: "4xl", fontStyle: "italic"})}>Memory Cards</h1>
+    <header className={cx(css({w: "full", display: "flex", py: "2", shadow: "7px 7px 0px 1px black", border: "3px solid black", justifyContent: "space-between", alignItems: "center"}), container())}>
+      <h1 className={css({color: "white", fontWeight: "bold", fontSize: "lg",md: {fontSize: "4xl"}, fontStyle: "italic"})}>Memory Cards</h1>
       <div className={css({display: "flex", gap: "4"})}>
         <ScoreBoard variant="muted">
           <ScoreBoard.Title>Remaining</ScoreBoard.Title>
